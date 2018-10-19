fdb59422b6ccfc3c5fdc7208838409dc1dc25e28
Danul De Leon <danul.deleon@gmail.com>
2018-10-19 09:52:20 -0700

    information is displaying from json file

--- LAB08-LINQ/Program.cs
+++ LAB08-LINQ/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LAB08_LINQ.Classes;
+using Newtonsoft.Json;
 
 namespace LAB08_LINQ
 {
@@ -7,6 +12,28 @@ namespace LAB08_LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            JsonQueries();
+        }
+
+        static void JsonQueries()
+        {
+            string path = "../../../data.json";
+            string text = "";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            var objects = JsonConvert.DeserializeObject<RootObject>(text);
+
+            Console.WriteLine("===========");
+
+            var neighborhoodsInManhatten = objects.features.Select(x => x).Select(x => x.properties.neighborhood);
+
+            foreach(var thing in neighborhoodsInManhatten){
+                Console.WriteLine(thing);
+            }
         }
     }
 }
