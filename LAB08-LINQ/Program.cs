6a3bdf5f0d18c75cf27b503544dd1e8909c93a50
Danul De Leon <danul.deleon@gmail.com>
2018-10-19 10:34:08 -0700

    finsihed queries

--- LAB08-LINQ/Program.cs
+++ LAB08-LINQ/Program.cs
@@ -15,6 +15,9 @@ namespace LAB08_LINQ
             JsonQueries();
         }
 
+        /// <summary>
+        /// This method displays information from a json file using different LINQ queries and lambda statements.
+        /// </summary>
         static void JsonQueries()
         {
             string path = "../../../data.json";
@@ -25,14 +28,63 @@ namespace LAB08_LINQ
                 text = sr.ReadToEnd();
             }
 
-            var objects = JsonConvert.DeserializeObject<RootObject>(text);
+            var data = JsonConvert.DeserializeObject<RootObject>(text);
 
-            Console.WriteLine("===========");
+            Console.WriteLine();
+            Console.WriteLine("=========== All Neighborhoods Using Lambda Functions ============");
 
-            var neighborhoodsInManhatten = objects.features.Select(x => x).Select(x => x.properties.neighborhood);
+            //1. Output all of the neighborhoods in this data list
+            var neighborhoodsInManhatten = data.features.Select(x => x).Select(x => x.properties.neighborhood);
 
-            foreach(var thing in neighborhoodsInManhatten){
-                Console.WriteLine(thing);
+            foreach(var neighborhood in neighborhoodsInManhatten){
+                Console.WriteLine(neighborhood);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=========== All Neighborhoods w/Names Using Lambda Functions ============");
+
+            //2. Filter out all the neighborhoods that do not have any names
+            var neighborhoodsNotNullInManhatten = neighborhoodsInManhatten.Select(x => x).Where(x => x != "");
+
+            foreach (var neighborhood in neighborhoodsNotNullInManhatten)
+            {
+                Console.WriteLine(neighborhood);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=========== All Neighborhoods w/o Duplicates Using Lambda Functions ============");
+
+            //3. Remove the Duplicates
+            var neighborhoodsNoDupsInManhatten = neighborhoodsNotNullInManhatten.Select(x => x).Distinct();
+
+            foreach (var neighborhood in neighborhoodsNoDupsInManhatten)
+            {
+                Console.WriteLine(neighborhood);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=========== Same thing but all chained together ============");
+
+            //4. Remove the Duplicates
+            var neighborhoodsChained = data.features.Select(x => x).Select(x => x.properties.neighborhood).Where(x=>x!="").Distinct();
+
+            foreach (var neighborhood in neighborhoodsChained)
+            {
+                Console.WriteLine(neighborhood);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=========== Same thing but using LINQ ============");
+
+            //5. Remove the Duplicates
+            var neighborhoodsWithLINQ = (from neighborhoods in data.features
+                                        where neighborhoods.properties.neighborhood != ""
+                                        select neighborhoods.properties.neighborhood).Distinct();
+
+
+            foreach (var neighborhood in neighborhoodsWithLINQ)
+            {
+                Console.WriteLine(neighborhood);
             }
         }
     }
